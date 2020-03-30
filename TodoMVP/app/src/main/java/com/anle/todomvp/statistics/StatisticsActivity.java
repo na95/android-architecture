b1753f81f044ed4bf48fdd623307ebb31809d32d
An Le <na95.ct@gmail.com>
2020-03-30 17:42:29 +0700

    add drawer menu

--- TodoMVP/app/src/main/java/com/anle/todomvp/statistics/StatisticsActivity.java
+++ TodoMVP/app/src/main/java/com/anle/todomvp/statistics/StatisticsActivity.java
@@ -3,13 +3,22 @@ package com.anle.todomvp.statistics;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.anle.todomvp.R;
+import com.anle.todomvp.tasks.TasksActivity;
 import com.anle.todomvp.util.ActivityUtils;
+import com.google.android.material.navigation.NavigationView;
 
 public class StatisticsActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +35,48 @@ public class StatisticsActivity extends AppCompatActivity {
 
         ActivityUtils.addFragmentToActivity(getSupportFragmentManager(),
                 StatisticsFragment.newInstance(), R.id.contentFrame);
+        // Set up the navigation drawer.
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.list_navigation_menu_item:
+                                Intent intent =
+                                        new Intent(StatisticsActivity.this, TasksActivity.class);
+                                startActivity(intent);
+                                break;
+                            case R.id.statistics_navigation_menu_item:
+                                // Do nothing, we're already on that screen
+                                break;
+                            default:
+                                break;
+                        }
+                        // Close the navigation drawer when an item is selected.
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Open the navigation drawer when the home icon is selected from the toolbar.
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- TodoMVP/app/src/main/java/com/anle/todomvp/tasks/TasksActivity.java
+++ TodoMVP/app/src/main/java/com/anle/todomvp/tasks/TasksActivity.java
@@ -3,13 +3,21 @@ package com.anle.todomvp.tasks;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.anle.todomvp.statistics.StatisticsActivity;
 import com.anle.todomvp.util.ActivityUtils;
 import com.anle.todomvp.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class TasksActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +35,49 @@ public class TasksActivity extends AppCompatActivity {
             ActivityUtils.addFragmentToActivity(getSupportFragmentManager(),
                     TasksFragment.newInstance(), R.id.contentFrame);
         }
+
+        // Set up the navigation drawer.
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        if (navigationView != null) {
+           setupDrawerContent(navigationView);
+        }
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.list_navigation_menu_item:
+                                // Do nothing, we're already on that screen
+                                break;
+                            case R.id.statistics_navigation_menu_item:
+                                Intent intent =
+                                        new Intent(TasksActivity.this, StatisticsActivity.class);
+                                startActivity(intent);
+                                break;
+                            default:
+                                break;
+                        }
+                        // Close the navigation drawer when an item is selected.
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Open the navigation drawer when the home icon is selected from the toolbar.
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
